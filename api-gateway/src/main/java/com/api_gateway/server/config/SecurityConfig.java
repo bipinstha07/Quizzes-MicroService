dfa77f26c2d55f5685021565271360818af2c9c4
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-08-24 01:11:58 -0500

    OAUTH 2.0

---
+++ api-gateway/src/main/java/com/api_gateway/server/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.api_gateway.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        http.cors(Customizer.withDefaults())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(config->config.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+
+}
