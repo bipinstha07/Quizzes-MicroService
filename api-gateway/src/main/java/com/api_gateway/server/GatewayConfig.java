d4950c8ff46a8fdf2784fc8febbca8af91632b27
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-08-31 22:39:30 -0500

    Rate limiter and Redis

--- api-gateway/src/main/java/com/api_gateway/server/GatewayConfig.java
+++ api-gateway/src/main/java/com/api_gateway/server/GatewayConfig.java
@@ -1,10 +1,13 @@
 package com.api_gateway.server;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -17,6 +20,7 @@ public class GatewayConfig {
                 .route("category-service",route -> route.path("/category/**")
                         .filters(f->f.rewritePath("/category/?(?<remaining>.*)","/${remaining}")
                                 .circuitBreaker(c->c.setName("categoryCB").setFallbackUri("forward:/categoryServiceFallback"))
+                                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(keyResolver()))
 
                         )
                         .uri("lb://CATEGORY-SERVICE")
@@ -27,9 +31,23 @@ public class GatewayConfig {
                                         retryConfig.setMethods(HttpMethod.GET)
                                         .setRetries(3)
                                         .setBackoff(Duration.ofMillis(50),Duration.ofMillis(600),2,true)
-                                ))
+                                )
+                                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(keyResolver()))
+                        )
                         .uri("lb://QUIZESMONGO"))
                 .build();
+    }
+
+
+
+    @Bean
+    public KeyResolver keyResolver(){
+        return exchange -> Mono.just(exchange.getRequest().getHeaders().getHost().getHostName());
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter(){
+        return new RedisRateLimiter(1,1);
     }
 
 }
